b55769ba6c235f20adba09a39ab801c0a8fb2521
zjjbk <116531427+zjjbk@users.noreply.github.com>
2023-08-17 17:32:29 +0800

    keep some footer

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -19,6 +19,19 @@ const Footer = ({ title }) => {
             className='relative z-10 flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'
         >
 
+              {/* 颜色过度区 */}
+            <div id='color-transition' className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'>
+
+            </div>
+
+            {/* 社交按钮 */}
+            <div className='w-full h-24'>
+                <SocialButton />
+            </div>
+
+            <AdSlot type='native'/>
+            <br />
+
             
         </footer >
   )
