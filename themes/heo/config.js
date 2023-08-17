81540e4ea7528c49878283ebc9999304cee9dd5e
zjjbk <116531427+zjjbk@users.noreply.github.com>
2023-08-17 11:30:18 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   HERO_CATEGORY_3: { title: 'Practical Tutorials', url: '/tag/Practical-Tutorials' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HERO_RECOMMEND_POST_TAG: 'Must-Read-Selection',
+  HERO_RECOMMEND_POST_TAG: 'recommend', // Must-Read-Selection
   HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
