abe068091dc7a18e9cf95b4d834be4f8bb5f4b59
zjjbk <116531427+zjjbk@users.noreply.github.com>
2023-08-21 15:55:46 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,8 +4,7 @@ const CONFIG = {
   SITE_CREATE_TIME: '2023-03-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  NOTICE_BAR: [
-    //{ title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+  NOTICE_BAR: [    
     //{ title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
